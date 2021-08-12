40ce428ee0794c751ee5f49f323926c702cc4949
고영남 <goyn95@naver.com>
2021-08-12 23:31:29 +0900

    update using while and if

--- src/Calender.java
+++ src/Calender.java
@@ -20,17 +20,22 @@ public class Calender {
 	public static void main(String[] args) {
 
 		// 숫자를 입력받아 해당하는 달의 최대 일수를 출력하는 프로그램
-		// System.out.println("알고 싶은 달을 입력하세요.: ");
+		
+		String PROMPT="cal>";
 		Scanner scan = new Scanner(System.in);
-		// int month = scan.nextInt();
 		Calender cal = new Calender();
-
-		System.out.println("반복횟수를 입력하세요.");
-		int repeat = scan.nextInt();
-
-		for (int i = 0; i < repeat; i++) {
+			
+		int month = 1;
+		while(true) {
 			System.out.println("달을 입력하세요.");
-			int month = scan.nextInt();
+			System.out.print(PROMPT);
+			month = scan.nextInt();
+			if (month == -1) {
+				break;
+			}
+			if(month > 12) {
+				continue;
+			}
 			System.out.printf("%d월은 %d일까지 있습니다.\n", month, cal.getMaxDaysOfMonth(month));
 		}
 		System.out.println("끝!");

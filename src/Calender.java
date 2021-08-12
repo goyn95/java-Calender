54bff860014f47d9c6ca13f3631f59b3f124f8c2
고영남 <goyn95@naver.com>
2021-08-12 22:19:41 +0900

    내가 한거
    면접준비하면서도 꾸준히하

--- src/Calender.java
+++ src/Calender.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class Calender {
 	public static void main(String[] args) {
@@ -7,6 +8,19 @@ public class Calender {
 		System.out.println("8  9 10 11 12 13 14");
 		System.out.println("15 16 17 18 19 20 21");
 		System.out.println("22 23 24 25 26 27 28");
+
+		// 숫자를 입력받아 해당하는 달의 최대 일수를 출력하는 프로그램
+		System.out.println("알고 싶은 달을 입력하세요.: ");
+		Scanner scan = new Scanner(System.in);
+		int inputValue = scan.nextInt();
+		
+		if(inputValue == 1 || inputValue == 3|| inputValue == 5 || inputValue == 7 || inputValue == 8|| inputValue == 10 || inputValue == 12) {
+			System.out.println(31);
+		}else if(inputValue == 2) {
+			System.out.println(28);
+		}else {
+			System.out.println(30);
+		}
 	}
 
 }
